cdd38f6e69fbfaabacf25920182525f74ab96308
Thomas Peterson <tap6@email.sc.edu>
2024-08-22 09:58:32 -0400

    Privated notes to different repository

--- csce145/src/edu/usc/csce145/chapter05/Shapes/Shapes_RectangleInterface.java
+++
@@ -1,9 +0,0 @@
-package edu.usc.csce145.chapter05.Shapes;
-
-public interface Shapes_RectangleInterface extends Shapes_ShapesInterface {
-
-	public int getHeight();
-	public int getWidth();
-	public void set(int xHeight, int xWidth);
-	
-}//EOC Shapes_RectangleInterface
--- csce145/src/edu/usc/csce145/chapter05/Shapes/Shapes_TriangleInterface.java
+++
@@ -1,8 +0,0 @@
-package edu.usc.csce145.chapter05.Shapes;
-
-public interface Shapes_TriangleInterface extends Shapes_ShapesInterface {
-	
-	public int getBase();
-	public void setBase(int xBase);
-
-}//EOC Shapes_TriangleInterface
